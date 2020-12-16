b0d210c94c9bbd033fde487e444106572204cc19
rick <rick@jenkins-zh.cn>
2020-12-16 16:18:11 +0800

    Add a common request context for other requests

--- src/main/java/io/kubesphere/sig/demo/web/SimpleWeb.java
+++ src/main/java/io/kubesphere/sig/demo/web/SimpleWeb.java
@@ -10,11 +10,24 @@ import java.net.InetSocketAddress;
 
 public class SimpleWeb {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress("", 8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
         server.createContext("/hello", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
-                final String simpleContent = "hellO";
+                final String simpleContent = "hello";
+                System.out.println("receive request");
+
+                try (OutputStream out = exchange.getResponseBody()) {
+                    exchange.sendResponseHeaders(200, simpleContent.length());
+                    out.write(simpleContent.getBytes());
+                }
+            }
+        });
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                final String simpleContent = "other request";
+                System.out.println("receive other request");
 
                 try (OutputStream out = exchange.getResponseBody()) {
                     exchange.sendResponseHeaders(200, simpleContent.length());
